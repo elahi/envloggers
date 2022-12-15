35a13f7fdb044cf1bce51b4084bc5bba7a4a0e2e
Robin Elahi <elahi.robin@gmail.com>
2022-12-14 20:44:27 -0800

    made time series and boxplot figs of hewatt env

---
+++ 2_analyze_env/analyze_hewatt_env_time_series.R
@@ -0,0 +1,103 @@
+################################################################################
+##' @title Analyze Hewatt temperature data
+##' @author Robin Elahi
+##' @date 2022-12-14
+##' @log 
+################################################################################
+
+#### PACKAGES, DATA ####
+library(here)
+library(tidyverse)
+library(lubridate)
+
+theme_set(theme_bw(base_size = 10) + 
+            theme(panel.grid = element_blank(), 
+                  strip.background = element_blank()))
+
+# Hewatt high zone
+d1 <- read_csv("data_output/envlogger_04924E000C5503.csv") %>% 
+  mutate(zone = "high")
+d1 %>% 
+  head(n = 1000) %>% 
+  ggplot(aes(time, temp)) + 
+  geom_line()
+
+# Hewatt mid zone
+d2 <- read_csv("data_output/envlogger_04EB240037540B.csv") %>% 
+  mutate(zone = "mid")
+d2 %>% 
+  head(n = 1000) %>% 
+  ggplot(aes(time, temp)) + 
+  geom_line()
+
+# Combine
+d <- rbind(d1, d2)
+
+# Lubridate
+d <- d %>% 
+  mutate(date = date(time), 
+         year = year(time), 
+         month = month(time), 
+         week = week(time), 
+         jday = yday(time))
+
+# Plot raw data
+d %>% 
+  tail() %>% 
+  ggplot(aes(time, temp, color = year)) + 
+  facet_wrap(~ zone) +
+  geom_line()
+
+#### QUALITY CONTROL ####
+# Need to remove days without a near-complete set of observations
+# Loggers recorded every 10 minutes, only use days with 95% of n_max
+n_max <- 24 * 60 / 10
+n_threshold <- 0.95 * n_max
+n_threshold
+
+d %>% count(date)
+
+d <- d %>% 
+  group_by(date, zone) %>%
+  mutate(n_obs = n()) %>% 
+  ungroup()
+
+unique(d$n_obs)
+
+d <- d %>% 
+  filter(n_obs > n_threshold)
+
+#### SUMMARIZE DAILY TEMPERATURE ####
+
+d_summary <- d %>%
+  group_by(year, month, week, jday, date, zone) %>% 
+  summarize(mean = mean(temp), 
+            max = max(temp), 
+            min = min(temp)) 
+
+ds_long <- d_summary %>% 
+  pivot_longer(cols = mean:min, names_to = "metric", values_to = "tempC")
+
+ds_long %>% 
+  ggplot(aes(date, tempC, color = metric)) + 
+  facet_wrap(~ zone) + 
+  geom_vline(xintercept = date("2022-01-01"), color = "gray", linetype = "dashed") + 
+  geom_line() + 
+  theme(legend.position = "top", 
+        legend.title = element_blank()) +
+  ylab(expression(paste("Temperature (", degree, "C)"))) + 
+  xlab("Date") 
+
+ggsave(here("figs", "hewatt_env_time_series.pdf"), 
+       height = 4, width = 7)
+
+ds_long %>% 
+  ggplot(aes(zone, tempC)) + 
+  facet_wrap(~ metric, scales = "free_y") + 
+  geom_violin(color = "gray") + 
+  geom_boxplot(width = 0.2, outlier.size = -1) + 
+  ylab(expression(paste("Temperature (", degree, "C)"))) + 
+  xlab("Zone")
+
+ggsave(here("figs", "hewatt_env_boxplot.pdf"), 
+       height = 4, width = 7)
