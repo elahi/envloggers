bfc02df925fb8b9b6bd85a46b96ca7dbbaf27d47
Robin Elahi <elahi.robin@gmail.com>
2022-12-14 19:03:50 -0800

    started analysis

--- 2_analyze_env/analyze_hewatt.R
+++ 2_analyze_env/analyze_hewatt.R
@@ -15,18 +15,33 @@ theme_set(theme_bw(base_size = 10) +
                   strip.background = element_blank()))
 
 # Hewatt high zone
-d1 <- read_csv("data_output/envlogger_04924E000C5503.csv")
+d1 <- read_csv("data_output/envlogger_04924E000C5503.csv") %>% 
+  mutate(zone = "high")
 
 # Hewatt low zone
-d2 <- read_csv("data_output/envlogger_04EB240037540B.csv")
+d2 <- read_csv("data_output/envlogger_04EB240037540B.csv") %>% 
+  mutate(zone = "low")
 
 # Combine
 d <- rbind(d1, d2)
 
+# Lubridate
+d <- d %>% 
+  mutate(date = date(time), 
+         year = year(time), 
+         month = month(time), 
+         week = week(time), 
+         jday = yday(time))
+
 # Plot raw data
 d %>% 
-  ggplot(aes(time, temp, color = serial)) + 
-  facet_wrap(~ serial) +
+  ggplot(aes(time, temp, color = year)) + 
+  facet_wrap(~ zone) +
+  geom_line()
+
+d %>% 
+  ggplot(aes(jday, temp, color = year)) + 
+  facet_wrap(~ zone) +
   geom_line()
 
 #### SUMMARIZE DAILY TEMPERATURE ####
