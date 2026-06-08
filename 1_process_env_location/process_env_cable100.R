5f29ac3f64d73ab3fffd57b2ee3c69884dd643f0
elahi <robin.elahi@gmail.com>
2026-06-08 12:07:55 -0700

    finished cable_100

---
+++ 1_process_env_location/process_env_cable100.R
@@ -0,0 +1,81 @@
+################################################################################
+##' @title EnvLogger processing - by location
+##' @author Robin Elahi
+##' @date 2026-06-08
+##' @log 
+################################################################################
+
+my_location <- "cable100"
+##' Location: HMS cable, 090m
+##' Logger serial: 0402 D400 A904 08 (n = 1)
+##' Note that before 2025, # of lines to skip is 20
+##' After 2025, # of lines to skip is 21
+##' Filter after 12pm of deployment date (check that this is ok!)
+##' Filter before 9am of collection date (check that this is ok!)
+
+#### PACKAGES, DATA ####
+library(here)
+library(tidyverse)
+source("R/envlogger_functions.R")
+
+#### DEPLOYMENT 1 ####
+
+# Get file names
+files <- list.files(path = "data/0402 D400 A904 08", pattern = "\\.csv")
+files
+i <- 1 # first file only
+
+# Folder with files
+serial <- env_file_parse(files[i])
+serial2 <- gsub(" ", "", serial, fixed = TRUE)
+
+d <- env_file_load(i = 1, my_files = files, my_skip = 20)
+d |> 
+  ggplot(aes(time, temp)) + geom_point(alpha = 0.5)
+
+head(d)
+d |> 
+  filter(time > as.POSIXct("2021-04-14 12:00:00")) |> 
+  ggplot(aes(time, temp)) + geom_point(alpha = 0.5)
+
+tail(d)
+d |> 
+  slice_tail(n = 500) |> 
+  filter(time < as.POSIXct("2021-06-18 09:00:00")) |> 
+  ggplot(aes(time, temp)) + geom_point(alpha = 0.5)
+
+d_trimmed <- d |>
+  filter(time > as.POSIXct("2021-04-14 12:00:00")) |> 
+  filter(time < as.POSIXct("2021-06-18 09:00:00"))
+  
+d_trimmed |> 
+  ggplot(aes(time, temp)) + geom_point(alpha = 0.5)
+
+# Rename
+d1 <- d_trimmed |> 
+  mutate(file_i = as.character(1), 
+         serial = serial)
+
+##### QUALITY CONTROL ####
+d <- rbind(d1)
+
+# Check for duplicates
+d %>% count(file_i, serial)
+
+d %>% 
+  filter(!is.na(temp)) %>% 
+  group_by(file_i) %>% 
+  summarise(mean = mean(temp)) %>% 
+  as.data.frame()
+
+d %>% 
+  ggplot(aes(time, temp, color = file_i)) + 
+  geom_line() + 
+  facet_wrap(~ file_i)
+
+d %>% 
+  ggplot(aes(time, temp, color = file_i)) + 
+  geom_line()
+
+##### WRITE PROCESSED FILE ####
+write_csv(d, here("data_output", paste("envlogger_", my_location, ".csv", sep = "")))
